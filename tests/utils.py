184fec0ac2cbae1fcf9ba2338b74735af885fba7
Andy Landy <none@none.com>
2024-10-08 17:28:34 +0000

    guthub testing fix

--- tests/utils.py
+++ tests/utils.py
@@ -41,12 +41,12 @@ def match_text_in_file(path: str, text: str) -> None:
 
     else:
         with open(path, 'r') as in_:
-            new_text = in_.read()
-            if text != new_text:
-                print('left =', text)
-                print('right =', new_text)
+            old_text = in_.read()
+            if text != old_text:
+                print('new =', text)
+                print('old =', old_text)
 
-            assert text == new_text
+            assert text == old_text
 
 
 def strip_tb_text(text: str) -> str:
